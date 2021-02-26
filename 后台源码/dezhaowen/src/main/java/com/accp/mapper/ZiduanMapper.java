b11acf99b94c864ecc65ee42bea8349561da56ce
linzihao888 <3500214366@qq.com>
2021-02-26 11:53:54 +0800

    cxz

---
+++ 后台源码/dezhaowen/src/main/java/com/accp/mapper/ZiduanMapper.java
@@ -0,0 +1,16 @@
+package com.accp.mapper;
+
+import com.accp.domain.Ziduan;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ytang
+ * @since 2021-02-26
+ */
+public interface ZiduanMapper extends BaseMapper<Ziduan> {
+
+}
---
+++ 后台源码/dezhaowen/src/main/java/com/accp/service/IZiduanService.java
@@ -0,0 +1,16 @@
+package com.accp.service;
+
+import com.accp.domain.Ziduan;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author ytang
+ * @since 2021-02-26
+ */
+public interface IZiduanService extends IService<Ziduan> {
+
+}
